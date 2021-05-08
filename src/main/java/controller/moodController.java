07c5ed6434fd895fba65449fea522db65782fd8f
Nagy Evelin <evelinnagy11@gmail.com>
2021-05-08 21:01:47 +0200

    Added FXMLLoader to saveMood method.

--- src/main/java/controller/moodController.java
+++ src/main/java/controller/moodController.java
@@ -2,12 +2,19 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import model.mood;
+import org.tinylog.Logger;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -55,7 +62,12 @@ public class moodController {
         saveButton.setVisible(true);
     }
 
-    public void saveMood(ActionEvent actionEvent) {
-
+    public void saveMood(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/activitiespage.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+        Logger.info("The mood is saved");
     }
 }
